93d15e838fd8be8fa7647d09c4c6fc87a00f754e
Chanachon <psho300@gmail.com>
2020-11-20 14:41:06 +0700

    domenu2

--- Program/Menu2.cpp
+++ Program/Menu2.cpp
@@ -1,19 +1,24 @@
 #include "income.h"
 
 //Check line 31, 42 & 57
-
+/*
 typedef struct
 {
 	char dd[10],mm[10];
 	int yy;
 }date;
-
+*/
 void Menu2()
 {
 	int menu2,i;
 	char filename[25] = "storage/";
+	char list_type_income [8][20] = {"Refund","Special","Revenue","Free","Business income","Withdraw","Borrow","Other"};
+	char list_type_expense [7][20] = {"Food","Transport","Accommodation","Groceries","Services","Utilities","Others"};
+	float amount[7] = {0};
+	//char Name[Type] = "Income Detail","Expense Detail";
 	FILE *p;
-	date date1;		
+	//date date1;
+			
 	
 	do{
 	
@@ -28,42 +33,44 @@ void Menu2()
 			if(!(Valid_daymonth(day,month))) printf("Invalid choice. Please try again.\n");
 		}while(!(Valid_daymonth(day,month)));
 		year = InvalidInput("Year  : ",0,INT_MAX);
+	
 		sprintf(filename,"storage/%02d-%02d-%d.txt",day,month,year);//Check filname correctly
-		
-		//filename = ("%02d%02d%d",day,month,year); 
+	
+		//filename = ("storage/%02d%02d%d",day,month,year); 
 	
 	if((p=fopen(filename,"r"))==NULL)
-	{
+/*	{
 		printf("Cannot open file \n");
-		//return ;
-	}
+		
+	}*/
+	
 	while (!feof(p)) 
 	{
-		fscanf(p,"storage/%02d-%02d-%d.txt",&day,&month,&year);//Look file and Menu1 that how information arrange.
+		fscanf(p,"storage/%02d-%02d-%din.txt",&day,&month,&year);//Look file and Menu1 that how information arrange.
 		//if(date==filename)
 		printf("\nIncome.\n");
-		printf("==================================\n");
-		printf("== Time ==    List    == Amount ==\n");
-		printf("==================================\n");
-		printf("== %02d   ==    %02d      ==  %d     ==\n",day,month,year);
-		printf("==================================\n");
-		printf("=====    Total     =====   %d    ==\n");
-		printf("==================================\n");
+		printf("======================================\n");
+		printf("==     List    == Amount == Detail  ==\n");
+		printf("======================================\n");
+		printf("==       %s    ==  %.2f  == %s      ==\n",list_type_income[i],amount[i],[Type]Name Income Detail);
+		printf("======================================\n");
+		printf("=====    Total   =========    %.2f    ==\n");
+		printf("======================================\n");
 		break;
 	}
 
 	while (!feof(p)) 
 	{
-		fscanf(p,"storage/%02d-%02d-%d.txt",&day,&month,&year);//Look file and Menu1 that how information arrange.
+		fscanf(p,"storage/%02d-%02d-%dout.txt",&day,&month,&year);//Look file and Menu1 that how information arrange.
 		//if(date==filename)
 		printf("\nExpense.\n");
-		printf("==================================\n");
-		printf("== Time ==    List    == Amount ==\n");
-		printf("==================================\n");
-		printf("== %02d   ==    %02d      ==  %d     ==\n",day,month,year);
-		printf("==================================\n");
-		printf("=====    Total     =====   %d    ==\n");
-		printf("==================================\n");
+		printf("======================================\n");
+		printf("==     List    == Amount == Detail  ==\n");
+		printf("======================================\n");
+		printf("==       %s    ==  %.2f  == %s      ==\n",list_type_expense[i],amount[i],[Type]Name Expense Detail);
+		printf("======================================\n");
+		printf("=====    Total   =========    %.2f    ==\n");
+		printf("======================================\n");
 		break;
 	}
 	
