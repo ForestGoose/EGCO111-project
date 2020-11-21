41b21372cb822638b789e9d5e47325288cba0244
Chanachon <psho300@gmail.com>
2020-11-21 14:09:19 +0700

    domenu2

--- Program/Menu2.cpp
+++ Program/Menu2.cpp
@@ -1,13 +1,4 @@
 #include "income.h"
-
-//Check line 31, 42 & 57
-/*
-typedef struct
-{
-	char dd[10],mm[10];
-	int yy;
-}date;
-*/
 void Menu2()
 {
 	int menu2,i;
@@ -15,10 +6,9 @@ void Menu2()
 	char list_type_income [8][20] = {"Refund","Special","Revenue","Free","Business income","Withdraw","Borrow","Other"};
 	char list_type_expense [7][20] = {"Food","Transport","Accommodation","Groceries","Services","Utilities","Others"};
 	float amount[7] = {0};
-	//char Name[Type] = "Income Detail","Expense Detail";
-	FILE *p;
-	//date date1;
-			
+	FILE *p1,*p2,*p3;
+	
+	struct list dis;
 	
 	do{
 	
@@ -26,56 +16,47 @@ void Menu2()
 
 	//Fill date.
 		int day,month,year;
-		printf("Plese Enter Date (Day-Month-Year):\n");
+		printf("Plese Enter Date (Day-Month-Year)\n");
 		do{
-			day = InvalidInput("Day   :",1,31);
+			day = InvalidInput("Day   : ",1,31);
 			month = InvalidInput("Month : ",1,12);
 			if(!(Valid_daymonth(day,month))) printf("Invalid choice. Please try again.\n");
 		}while(!(Valid_daymonth(day,month)));
 		year = InvalidInput("Year  : ",0,INT_MAX);
 	
-		sprintf(filename,"storage/%02d-%02d-%d.txt",day,month,year);//Check filname correctly
+		sprintf(filename,"storage/%02d-%02d-%din.txt",day,month,year);
+		sprintf(filename,"storage/%02d-%02d-%dout.txt",day,month,year);
+		sprintf(filename,"storage/%dtotal.txt",year);
 	
-		//filename = ("storage/%02d%02d%d",day,month,year); 
-	
-	if((p=fopen(filename,"r"))==NULL)
-/*	{
-		printf("Cannot open file \n");
-		
-	}*/
-	
-	while (!feof(p)) 
-	{
-		fscanf(p,"storage/%02d-%02d-%din.txt",&day,&month,&year);//Look file and Menu1 that how information arrange.
-		//if(date==filename)
-		printf("\nIncome.\n");
-		printf("======================================\n");
-		printf("==     List    == Amount == Detail  ==\n");
-		printf("======================================\n");
-		printf("==       %s    ==  %.2f  == %s      ==\n",list_type_income[i],amount[i],[Type]Name Income Detail);
-		printf("======================================\n");
-		printf("=====    Total   =========    %.2f    ==\n");
-		printf("======================================\n");
-		break;
-	}
+		printf("====================================================\n");
+		printf("==     List       == Income == Expense ==  Detail ==\n");
+		printf("====================================================\n");
 
-	while (!feof(p)) 
+	if((p1=fopen("storage/%02d-%02d-%din.txt","r"))==NULL)
+		
+	while (!feof(p1)) 
 	{
-		fscanf(p,"storage/%02d-%02d-%dout.txt",&day,&month,&year);//Look file and Menu1 that how information arrange.
-		//if(date==filename)
-		printf("\nExpense.\n");
-		printf("======================================\n");
-		printf("==     List    == Amount == Detail  ==\n");
-		printf("======================================\n");
-		printf("==       %s    ==  %.2f  == %s      ==\n",list_type_expense[i],amount[i],[Type]Name Expense Detail);
-		printf("======================================\n");
-		printf("=====    Total   =========    %.2f    ==\n");
-		printf("======================================\n");
-		break;
-	}
+		fscanf(p1,"%s %s %.2f %s",dis.name,dis.type,&dis.amount,dis.detail);
+		printf("==  [%s] %s       ==  %.2f  ==    -    ==    %s    ==\n",dis.name,dis.type,dis.amount,dis.detail);
+	}break;
+	fclose(p1);	
+		
+	if((p2=fopen("storage/%02d-%02d-%dout.txt","r"))==NULL)
 	
-	fclose(p);
-	
+	while (!feof(p2)) 
+	{
+		fscanf(p2,"%s %s %.2f %s",dis.name,dis.type,&dis.amount,dis.detail);	
+		printf("==  [%s] %s       ==   -    ==  %.2f   ==    %s    ==\n",dis.name,dis.type,dis.amount,dis.detail);
+	}break;	
+	fclose(p2);
+		
+		printf("====================================================\n");
+		
+	if((p3=fopen("storage/%dtotal.txt","r"))==NULL)
+		printf("=====  Total      ==  %.2f  ==  %.2f  ===============\n",p3);break;
+	fclose(p3);	
+		printf("====================================================\n");
+		
 
 	printf("\nDo you want to see other day (Press 1(Yes),0(No)) : ");
 	menu2 = InvalidInput(" ",0,1);
@@ -86,7 +67,7 @@ void Menu2()
 		default: printf("Back to Main Menu \n"); break;
 	}
 	delay(1000);
-		
+	
 	}while (menu2 !=0 );
 	
 }
